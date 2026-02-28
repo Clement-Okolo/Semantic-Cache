7e47c390159f2a0ba815a34f8c28b2cad72e87b9
Clement Okolo <65208835+Clement-Okolo@users.noreply.github.com>
2026-02-27 20:39:52 -0600

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,18 @@
+# ============================================================
+# secrets.py — store your credentials here.
+# NEVER commit this file to source control.
+# It is listed in .gitignore by default.
+# ============================================================
+
+LLAMA_CLOUD_API_KEY = "llx-lNLTRIRUugDW8t2D0qYIQIbW4jKW0a045zIMAQQ2IyK5Dbtg"
+
+GROQ_API_KEY = "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"
+
+REDIS_HOST = "redis-12807.c257.us-east-1-3.ec2.cloud.redislabs.com"
+REDIS_PORT = 12807
+REDIS_PASSWORD = "ZOtleOqSFaAllMTxCN5ihzjMRdMnHdqI"
+HF_TOKEN= "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"
+REDIS_URL = "redis-cli -u redis://default:ZOtleOqSFaAllMTxCN5ihzjMRdMnHdqI@redis-12807.c257.us-east-1-3.ec2.cloud.redislabs.com:12807"
+
+GCP_PROJECT_ID = "semantic-cache-project"
+GCP_REGION = "us-central1"
